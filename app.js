1b417b3d9719ed8e44a3d820b555270fdc4f2c97
Hillary Dreikorn <hillary@dreikorn.com>
2018-10-15 21:39:08 -0700

    Adding files and folder for set up

---
+++ app.js
@@ -0,0 +1,18 @@
+// Boilerplate
+var express = require("express");
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.use(express.static('public'));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', '8001');
+
+// Render Landing Page
+app.get('/', function(req,res){
+  res.render('home');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express has started on localhost!');
+});
