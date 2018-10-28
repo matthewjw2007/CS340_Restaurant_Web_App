193fcdf3879386ab8ffb5d5979e69471a5d96740
HDreikorn <32907903+HDreikorn@users.noreply.github.com>
2018-10-28 16:14:35 -0700

    added functions for button submits

---
+++ public/script.js
@@ -0,0 +1,23 @@
+// This function will set up the page for adding a restaurant to a list.
+function addRR() {
+  console.log("addRR entry called!");
+  location.href = "/addRR";
+}
+
+// This function will set up the page for adding a list.
+function addList() {
+  console.log("addList called!");
+  location.href = "/addList";
+}
+
+// This function will set up the page for find another user.
+function findFriend() {
+  console.log("findFriend called!");
+  location.href = "/findFriend";
+}
+
+// This function will set up the page for editing user account.
+function edit() {
+  console.log("edit called!");
+  location.href = "/edit";
+}
