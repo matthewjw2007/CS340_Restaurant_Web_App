fde41350cab11cceb3ceb8ab39f632dd324cd580
HDreikorn <32907903+HDreikorn@users.noreply.github.com>
2018-10-28 16:13:34 -0700

    updated with all pages and formatting

--- app.js
+++ app.js
@@ -8,17 +8,56 @@ app.use(express.static('public'));
 //Set the view engine
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', '8001');
+app.set('port', '53232');
 
 // Render Landing Page
 app.get('/', function(req,res){
-    //The below code is how to use my style .css sheets
-    app.use(express.static(__dirname + '/views'));
-    //Handlebars code if the user is logged out
-    const loggedOut = {loggedOut: true};
-    //Handlebars code if the user is logged in
-    //The below code is the .hbs file that will be rendered
-    res.render('home', loggedOut);
+    var context = {};
+    context.title = "Gastro Gnomes Homepage";
+    context.loggedOut = true;
+    res.render('home', context);
+});
+
+app.get('/lists', function(req,res){
+  var context = {};
+  context.title = "List Display";
+  res.render('lists', context);
+});
+
+app.get('/addRR', function(req,res){
+  var context = {};
+  context.title = "Add Restaurant";
+  res.render('addRR', context);
+});
+
+app.get('/addUsr', function(req,res){
+  var context = {};
+  context.title = "Add New User";
+  res.render('addUsr', context);
+});
+
+app.get('/addList', function(req,res){
+  var context = {};
+  context.title = "Add New List";
+  res.render('addList', context);
+});
+
+app.get('/findFriend', function(req,res){
+  var context = {};
+  context.title = "Find Friend";
+  res.render('findFriend', context);
+});
+
+app.get('/edit', function(req,res){
+  var context = {};
+  context.title = "Edit Account";
+  res.render('edit', context);
+});
+
+app.get('/about', function(req,res){
+  var context = {};
+  context.title = "About Gastro Gnomes";
+  res.render('about', context);
 });
 
 app.listen(app.get('port'), function(){
